3351e1de3a99feb862e3f4a7a2638cb3fa8c8903
Akabhishek067 <akabhishek067@gmail.com>
2022-04-24 21:13:07 +0530

    user registration using form builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-learning-project';
+
+  userDetailForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.userDetailForm = fb.group({
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  getUserDetails = () => {
+
+    console.log(this.userDetailForm.value);
+  }
+
 }
