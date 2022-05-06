dd09aaa53b1ef5f39d2548ad31f37358fba318c7
Akabhishek067 <akabhishek067@gmail.com>
2022-05-06 17:42:18 +0530

    router and wildcard router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from 'src/components/about/about.component';
+import { ContactComponent } from 'src/components/contact/contact.component';
+import { HomeComponent } from 'src/components/home/home.component';
+import { PageNotFoundComponent } from 'src/components/page-not-found/page-not-found.component';
+import { ProfileComponent } from 'src/components/profile/profile.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  //wildcard routing
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProfileComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
