c33769936c4d93e54ce824dc6de877a2776c88f5
Akabhishek067 <akabhishek067@gmail.com>
2022-06-11 10:50:26 +0530

    template reference variable on input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-learning-project';
+
+  showEmpDetail = (value: string) => {
+    console.log(value)
+  }
 }
