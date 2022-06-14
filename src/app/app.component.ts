c7224465c260a59ba80882190e0e030902fa86a2
Akabhishek067 <akabhishek067@gmail.com>
2022-06-14 20:32:00 +0530

    example to sanitize content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-learning-project';
+
+  malware = "Some Random Data receiving from API";
+
+  trustedContent: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) { 
+    this.trustedContent = this.sanitizer.bypassSecurityTrustHtml(this.malware);
+  }
 }
